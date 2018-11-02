aef5b9981c107eb4c78a3de715d8b49203eacfeb
Yamenhammodueh <mr.yamen@gmail.com>
2018-11-02 01:15:40 +0100

    Update build_test_db.sql

--- build/Events-master/test/build_test_db.sql
+++ build/Events-master/test/build_test_db.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS event_testdb /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+CREATE DATABASE IF NOT EXISTS event_testdb /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
 USE event_testdb;
 
@@ -9,15 +9,15 @@ CREATE TABLE IF NOT EXISTS events (
   meny_id int(10) unsigned NOT NULL,
   PRIMARY KEY (event_id),
   UNIQUE KEY event_id_UNIQUE (event_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
 TRUNCATE events;
 
 INSERT INTO events (event_id, user_id, event_date, meny_id) VALUES 
-  (1, 1, '2018-10-23', 5),
-  (2, 1, '2018-10-10', 3),
-  (3, 2, '2018-12-12', 5),
-  (4, 1, '2018-09-21', 6),
-  (6, 1, '2018-12-11', 2),
-  (7, 1, '2018-10-11', 6),
-  (8, 1, '2018-11-21', 2);
+  (1, 1, 2018-10-23, 5),
+  (2, 1, 2018-10-10, 3),
+  (3, 2, 2018-12-12, 5),
+  (4, 1, 2018-09-21, 6),
+  (6, 1, 2018-12-11, 2),
+  (7, 1, 2018-10-11, 6),
+  (8, 1, 2018-11-21, 2);
